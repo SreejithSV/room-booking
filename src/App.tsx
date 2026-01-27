9a8f45a40601d557411ceed27fba36f64e301e61
Sreejith S V <sreejith.the.prince@gmail.com>
2026-01-27 14:25:55 +0530

    basename

--- src/App.tsx
+++ src/App.tsx
@@ -17,7 +17,7 @@ const App = () => (
     <TooltipProvider>
       <Toaster />
       <Sonner />
-      <BrowserRouter>
+      <BrowserRouter basename="/room-booking/">
         <Routes>
           <Route path="/" element={<Index />} />
           <Route path="/admin" element={<AdminPortal />} />
