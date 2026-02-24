d6568f71f88564f031b4089bd1c4ebc382b7f709
Sreejith S V <sreejith.the.prince@gmail.com>
2026-02-24 23:01:37 +0530

    app.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -23,7 +23,7 @@ const App = () => (
     <TooltipProvider>
       <Toaster />
       <Sonner />
-      <BrowserRouter>
+      <BrowserRouter basename="amrodek/">
         <Navbar />
         <Routes>
           <Route path="/" element={<Index />} />
